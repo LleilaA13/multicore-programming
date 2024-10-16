97a135a56e28356f28a2f977a09a50c3e64a86fe
LleilaA13 <zanoni.2033176@studenti.uniroma1.it>
2024-10-16 20:28:23 +0200

    commit

--- lec6/argv.c
+++ lec6/argv.c
@@ -6,7 +6,7 @@ int main(int argc, char** argv){
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     printf("I am rank %d and argv[0] is %s argv[1] is %s\n", 
-           rank, argv[0], argv[1]);
+        rank, argv[0], argv[1]);
     MPI_Finalize();
     return 0;
 }
